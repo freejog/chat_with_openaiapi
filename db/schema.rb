762c2230c50628a50f4efb9d6614a621634fdee4
freejog <167161823+freejog@users.noreply.github.com>
2024-11-19 20:58:50 +0900

    スレッドのモデルとテーブルを用意
    ChatThread（スレッド）とMessage（チャット）は、1対多の関係

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_05_014225) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_19_115404) do
+  create_table "chat_threads", charset: "utf8mb4", force: :cascade do |t|
+    t.text "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "messages", charset: "utf8mb4", force: :cascade do |t|
     t.text "prompt"
     t.text "response"
